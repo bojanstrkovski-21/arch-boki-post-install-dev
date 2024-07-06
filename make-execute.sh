6b8c963ad51f330eb665fcd6405ce659887c8877
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-07-06 16:54:36 +0200

    .gitignore

---
+++ make-execute.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+chmod +x arch_boki_bash.sh
+chmod +x add-repos/*
+chmod +x choose-desktop/cinnamon/*
+chmod +x choose-desktop/dwl-dev/*
+chmod +x choose-desktop/hyprland-scripts/*
+chmod +x choose-desktop/kde-plasma/*
+chmod +x choose-desktop/xfce/*
+chmod +x core-utils/*
+chmod +x install-scripts/
