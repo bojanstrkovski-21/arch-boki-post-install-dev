0efc55416b10f06bfdd9f3c5591d1a2b5a14b5bd
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-07-19 21:13:23 +0200

    update reborn

--- add-repos/06-reborn-os.sh
+++ add-repos/06-reborn-os.sh
@@ -8,7 +8,7 @@ rebornos-keyring-20240525-1-any.pkg.tar.zst \
 rebornos-mirrorlist-20240513-1-any.pkg.tar.zst
 
 
-echo -e "\n[Reborn-OS]\nSigLevel = Optional TrustAll\nServer = /etc/pacman.d/reborn-mirrorlist" | sudo tee -a /etc/pacman.conf
+echo -e "\n[Reborn-OS]\nServer = /etc/pacman.d/reborn-mirrorlist" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syy
 
 # [Reborn-OS]
