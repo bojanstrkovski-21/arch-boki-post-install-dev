e45c398e3c872d5ee829347f1d6783712c029623
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-07-19 21:10:06 +0200

    append reborn

--- add-repos/06-reborn-os.sh
+++ add-repos/06-reborn-os.sh
@@ -6,3 +6,11 @@ cd reborn-os
 sudo pacman -U --needed --noconfirm \
 rebornos-keyring-20240525-1-any.pkg.tar.zst \
 rebornos-mirrorlist-20240513-1-any.pkg.tar.zst
+
+
+echo -e "\n[Reborn-OS]\nSigLevel = Optional TrustAll\nServer = /etc/pacman.d/reborn-mirrorlist" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syy
+
+# [Reborn-OS]
+# SigLevel = Optional TrustAll
+# Include = /etc/pacman.d/reborn-mirrorlist
