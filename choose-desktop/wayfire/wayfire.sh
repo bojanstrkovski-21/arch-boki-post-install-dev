8da3249ded0349fb9aff8f96e13da28d6d404531
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-07-20 21:20:36 +0200

    wayfire

---
+++ choose-desktop/wayfire/wayfire.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env/bash
+
+sudo pacman -S --needed --noconfirm \
+cairo \
+pango \
+freetype2 \
+lixft \
+glm \
+libdrm \
+libevdev \
+mesa \
+libinput \
+libjpeg-turbo \
+libpng \
+libxkbcommon \
+libxml2 \
+pixman \
+pkg-conf \
+wayland \
+wayland-protocols \
+wlroots \
+libdisplay-info \
+hwdata \
+libglvnd \
+mesa \
+libxcb \
+libxcomposite \
+librender \
+libxfixes \
+libx11 \
+xorg-xinput \
+feh \
+alacritty \
+foot \
+grim \
+kitty \
+nwg-look \
+mako \
+micro \
+pamixer \
+picom \
+pulsemixer \
+polkit-gnome \
+rofi-lbonn-wayland \
+slurp \
+swaybg \
+swayidle \
+swaylock \
+swww \
+thunar \
+thunar-archive-plugin \
+thunar-volman \
+ttf-jetbrains-mono-nerd \
+waybar-git \
+wayfire-git \
+wf-config \
+wayfire-plugins-extra-git \
+wcm-git \
+wf-kill-git \
+wf-shell-git \
+wl-clipboard \
+wofi \
+xdg-desktop-portal-wlr \
+xfce4-terminal \
+nwg-drawer-bin \
+a-candy-beauty-icon-theme-git \
+
+# arcolinux-nwg-drawer-git
+# archlinux-logout-git \
+# arcolinux-alacritty-git \
+# arcolinux-foot-git \
+# arcolinux-kitty-git \
+# arcolinux-pywal-cache-git \
+# arcolinux-rofi-git \
+# arcolinux-rofi-themes-git \
+# arcolinux-wallpapers-wayfire-git \
+# arcolinux-wayfire-git \
+# arcolinux-wayland-app-hooks-git \
+# arconet-xfce \
+# lxappearance \
