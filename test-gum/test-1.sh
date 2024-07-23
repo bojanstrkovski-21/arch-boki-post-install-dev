214f9881abc5c77bd9f7bc9b8eed50dc10b49966
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-07-23 19:52:33 +0200

    arch-boki-repos-gum

--- test-gum/test-1.sh
+++ test-gum/test-1.sh
@@ -73,8 +73,8 @@ elif [ "${update_refresh}" = "Add ArcoLinux_repos" ]; then
 elif [ "${update_refresh}" = "Add Chaotic_repos" ]; then 
 	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 	sudo p	acman-key --lsign-key 3056513887B78AEB
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+	sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 	echo -e "\n[garuda]\nSigLevel = Required DatabaseOptional\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 	echo -e "\n[chaotic-aur]\nSigLevel = Required DatabaseOptional\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 	sudo pacman -Syy
