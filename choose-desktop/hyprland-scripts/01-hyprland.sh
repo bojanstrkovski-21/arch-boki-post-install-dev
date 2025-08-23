fce15f380bf926310b558360df6a138d1ccda73e
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-08-23 23:55:47 +0200

    upd desktop hyprland

--- choose-desktop/hyprland-scripts/01-hyprland.sh
+++ choose-desktop/hyprland-scripts/01-hyprland.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm \
-hyprland \
-hyprpaper \
-hyprlock \
-wlogout \
-waybar \
+#sudo pacman -S --needed --noconfirm hyprpaper
+
+sudo pacman -S --needed --noconfirm hypridle
+sudo pacman -S --needed --noconfirm hyprland
+sudo pacman -S --needed --noconfirm hyprland-protocols
+sudo pacman -S --needed --noconfirm hyprlock
+sudo pacman -S --needed --noconfirm hyprpicker
+sudo pacman -S --needed --noconfirm hyprpolkitagent
+sudo pacman -S --needed --noconfirm hyprsunset
+sudo pacman -S --needed --noconfirm wlogout
+sudo pacman -S --needed --noconfirm waybar
